20b691ecda2755d699ffcde7635658d43bb601fa
Yujie Wang <79264663+Christol-Jalen@users.noreply.github.com>
2024-07-29 23:20:06 +0000

    Add files via upload

---
+++ OffroadPlanning/evaluation.m
@@ -0,0 +1,25 @@
+% Load data
+load('recording.mat')
+x1 = data{1}.Values.Data;
+x2 = data{2}.Values.Data;
+x3 = data{3}.Values.Data;
+x4 = paths_offset{3,1}(:, 1);
+y1 = data{4}.Values.Data;
+y2 = data{5}.Values.Data;
+y3 = data{6}.Values.Data;
+y4 = paths_offset{3,1}(:, 2);
+
+
+% Plot
+figure;
+plot(x1, y1, 'b', 'LineWidth', 2);% ground truth trajectory
+hold on;
+plot(x2, y2, 'r', 'LineWidth', 2);% amcl trajctory
+plot(x3, y3, 'k', 'LineWidth', 2);% odom trajctory
+plot(x4, y4, 'g', 'LineWidth', 2);% planned trajctory
+legend('ground truth', 'amcl', 'odometry', 'planned path');
+xlabel('X(m)');
+ylabel('Y(m)');
+title('Trajctory Evalutation');
+grid on;
+hold off;
